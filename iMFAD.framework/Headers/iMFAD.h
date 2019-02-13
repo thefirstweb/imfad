8c1ca0c90abd4cf4514833c9ca1ab12d198146c8
ck00031 <alexwang@sitetag.us>
2019-02-13 16:09:54 +0800

    first commit

---
+++ iMFAD.framework/Headers/iMFAD.h
@@ -0,0 +1,23 @@
+//
+//  iMFAD.h
+//  iMFAD
+//
+//  Created by Charles on 2017/8/28.
+//  Copyright © 2017年 Charles. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for iMFAD.
+FOUNDATION_EXPORT double iMFADVersionNumber;
+
+//! Project version string for iMFAD.
+FOUNDATION_EXPORT const unsigned char iMFADVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <iMFAD/PublicHeader.h>
+#import "MFBannerView.h"
+#import "MFInterstitialView.h"
+#import "MFNativeAd.h"
+
+#define ClickForce_AD_SDK_VERSION @"2.0.3"
+
